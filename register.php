61c8f8290216640e1c13890e78b1cee64debd69a
Al Aziz Com <Al Aziz Com@AlAzizCom-PC.Home>
2019-05-02 08:36:00 +0500

    rabia created register file

--- register.php
+++ register.php
@@ -5,8 +5,24 @@ include "header/header.php";?>
 
    
     <div class = "form-group">
+      <label for = "name">name</label>
+      <input type = "text" class = "form-control" name = "name" placeholder = "enter name ">
+   </div>
+    <div class = "form-group">
+      <label for = "name">cnic</label>
+      <input type = "text" class = "form-control" name = "cnic" placeholder = "enter cnic ">
+   </div>
+     <div class = "form-group">
+      <label for = "name">address</label>
+      <input type = "text" class = "form-control" name = "address" placeholder = "enter address">
+   </div>
+     <div class = "form-group">
+      <label for = "name">phoneno</label>
+      <input type = "text" class = "form-control" name = "phoneno" placeholder = "enter phoneno">
+   </div>
+     <div class = "form-group">
       <label for = "name">email</label>
-      <input type = "text" class = "form-control" name = "email" placeholder = "enter email">
+      <input type = "text" class = "form-control" name = "email" placeholder = "enter email ">
    </div>
    <div class = "form-group">
       <label for = "name">password</label>
