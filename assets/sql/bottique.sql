ea3ec6ccaa6d017b0b10a074a283520373aa75f9
MusawerAli <pakjalihouse@gmail.com>
2019-05-18 22:26:00 +0500

    sql

--- assets/sql/bottique.sql
+++ assets/sql/bottique.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: May 18, 2019 at 05:48 AM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.4
+-- Host: 127.0.0.1
+-- Generation Time: May 18, 2019 at 07:24 PM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,30 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `bottique`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bottiqueowner`
---
-
-CREATE TABLE `bottiqueowner` (
-  `id` int(11) NOT NULL,
-  `name` varchar(250) DEFAULT NULL,
-  `cnic` varchar(13) DEFAULT NULL,
-  `address` varchar(250) DEFAULT NULL,
-  `phoneno` int(12) DEFAULT NULL,
-  `image` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `bottiqueowner`
---
-
-INSERT INTO `bottiqueowner` (`id`, `name`, `cnic`, `address`, `phoneno`, `image`) VALUES
-(2, 'botique 1', '44', '544', 64, 'candidate_images/Jellyfish-jpg.jpg'),
-(3, 'botique 2', '4', '5', 6, 'candidate_images/Tulips-jpg.jpg'),
-(4, 'rabia', '345345', 'fgfdgdfg', 0, 'candidate_images/Penguins-jpg.jpg');
 
 -- --------------------------------------------------------
 
@@ -102,7 +78,45 @@ INSERT INTO `boutique_post` (`id`, `vendor_id`, `vendor_name`, `vendor_city`, `c
 (14, 1259, 'Musawer ALi', 'Bahawalpur', 3, '', 'Ladies Stritching Cloth', 'classic-vol1-collection.jpg', 20, 2999, '2019-05-17 13:10:44', '4 Season', 'Washingware (Summer)', '+92333651351'),
 (15, 1259, 'Musawer ALi', 'Bahawalpur', 5, '', 'Men Jeans', 'menscomingsoonpage-edit.jpg', 80, 1999, '2019-05-17 13:13:10', 'Orignal Dennim Branded', 'Dennim', '923336513516'),
 (16, 1260, 'Ch Musawer', 'Bahawalpur', 8, '', 'Pary suites', 'clothing.jpg', 50, 9999, '2019-05-17 14:57:49', 'Party Suites Branded', 'Tom Tailor', '923336513516'),
-(17, 1260, 'Ch Musawer', 'Bahawalpur', 9, '', 't-shirts', 'FI015-1920x1080-t-shirts-en.jpg', 1000, 399, '2019-05-17 14:59:18', 'Available All Colors\r\nGood Quality', 'Denim', '923336513516');
+(17, 1260, 'Ch Musawer', 'Bahawalpur', 9, '', 't-shirts', 'FI015-1920x1080-t-shirts-en.jpg', 1000, 399, '2019-05-17 14:59:18', 'Available All Colors\r\nGood Quality', 'Denim', '923336513516'),
+(18, 1259, 'Musawer ALi', 'Bahawalpur', 10, '', 'Wedding dresses', 'indian-wedding-suits-for-men-1.jpg', 100, 7999, '2019-05-18 14:01:06', 'All types of available', 'Branded', '923336513516'),
+(19, 1259, 'Musawer ALi', 'Bahawalpur', 11, '', '7 piece suite', 'Suit.png', 70, 5999, '2019-05-18 14:03:14', 'Suites for Men', 'L.Tailor', '923336513516');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `vendor_id` int(11) NOT NULL,
+  `customer_name` varchar(50) NOT NULL,
+  `address` text NOT NULL,
+  `user_cnic` varchar(50) NOT NULL,
+  `user_contact` varchar(50) NOT NULL,
+  `prd_name` varchar(50) NOT NULL,
+  `vendor_city` varchar(50) NOT NULL,
+  `prd_price` int(11) NOT NULL,
+  `prd_quality` varchar(50) NOT NULL,
+  `prd_qty` int(11) NOT NULL,
+  `prd_desc` text NOT NULL,
+  `vendor_contact` varchar(50) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `total` int(11) NOT NULL,
+  `prd_image` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`id`, `user_id`, `vendor_id`, `customer_name`, `address`, `user_cnic`, `user_contact`, `prd_name`, `vendor_city`, `prd_price`, `prd_quality`, `prd_qty`, `prd_desc`, `vendor_contact`, `date`, `total`, `prd_image`) VALUES
+(5, 1253, 1260, 'rabia is beautiful', '\"189/f\"', '31202-70111746-4', '03009687669', 't-shirts', 'Bahawalpur', 399, 'Denim', 6, 'Available All Colors\r\nGood Quality\"', '923336513516', '2019-05-18 17:23:25', 2394, 'FI015-1920x1080-t-shirts-en.jpg'),
+(6, 1261, 1259, 'Ali Raza', '\"Trust Colony,Bahawalpur\"', '3120269441941', '+92300123456', 'Blue Jeans', 'Bahawalpur', 999, 'Fiber Cotton', 2, '4 seasons\r\ntwo colors\r\n32 to 45 waste\"', '+923336513516', '2019-05-18 18:52:28', 1998, '13a5e9eee22ce15319cd14c9f28e582d.jpg'),
+(7, 1261, 1260, 'Ali Raza', '\"Trust Colony,Bahawalpur\"', '3120269441941', '+92300123456', 't-shirts', 'Bahawalpur', 399, 'Denim', 1, 'Available All Colors\r\nGood Quality\"', '923336513516', '2019-05-18 18:52:46', 399, 'FI015-1920x1080-t-shirts-en.jpg'),
+(8, 1261, 1259, 'Ali Raza', '\"Trust Colony,Bahawalpur\"', '3120269441941', '+92300123456', 'Leather Shoes (Orignal)', 'Bahawalpur', 2999, 'Upper Leather', 4, 'Upper Leather,\r\nRubber Sole,\r\nSIze:41 to 45\"', '+923336513516', '2019-05-18 18:53:17', 11996, '031-Shoes-to-wear-with-jeans.jpeg');
 
 -- --------------------------------------------------------
 
@@ -131,7 +145,9 @@ INSERT INTO `category` (`id`, `category_name`, `image`, `user_id`, `user_type`) 
 (6, 'Radimate', NULL, 1259, 'seller'),
 (7, 'shoes', NULL, 1259, 'seller'),
 (8, 'court', NULL, 1260, 'seller'),
-(9, 'shirts', NULL, 1260, 'seller');
+(9, 'shirts', NULL, 1260, 'seller'),
+(10, 'wedding dress', NULL, 1259, 'seller'),
+(11, 'suite', NULL, 1259, 'seller');
 
 -- --------------------------------------------------------
 
@@ -159,27 +175,8 @@ CREATE TABLE `customer` (
 INSERT INTO `customer` (`id`, `customer_name`, `cnic`, `address`, `mobile_number`, `email`, `password`, `image`, `type`, `city`) VALUES
 (1253, 'rabia is beautiful', '31202-70111746-4', '189/f', '03009687669', 'rabia100789@gmail.com', '03057799830', 'candidate_images/Chrysanthemum-jpg.jpg', 'user', NULL),
 (1259, 'Musawer ALi', '3120269441941', 'pak jali house, Near Shama Canima Multan Road Bahawalpur', '923336513516', 'pakjalihouse@yahoo.com', 'admin123', '36252369.jpeg', 'seller', 'Bahawalpur'),
-(1260, 'Ch Musawer', '3120269441941', 'pak jali house, Near Shama Canima Multan Road Bahawalpur', '923336513516', 'pakjalihouse@gmail.com', 'admin123', '79c2689a332e9946ade72debd15b45eb.jpg', 'seller', 'Bahawalpur');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `login`
---
-
-CREATE TABLE `login` (
-  `login_id` int(11) DEFAULT NULL,
-  `name` varchar(30) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `email` varchar(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `login`
---
-
-INSERT INTO `login` (`login_id`, `name`, `password`, `email`) VALUES
-(12, '', '', '');
+(1260, 'Ch Musawer', '3120269441941', 'pak jali house, Near Shama Canima Multan Road Bahawalpur', '923336513516', 'pakjalihouse@gmail.com', 'admin123', '79c2689a332e9946ade72debd15b45eb.jpg', 'seller', 'Bahawalpur'),
+(1261, 'Ali Raza', '3120269441941', 'Trust Colony,Bahawalpur', '+92300123456', 'aliraza.k2@gmail.com', 'aliraza123', 'Sample-1---right.jpg', 'user', 'Islamabad');
 
 -- --------------------------------------------------------
 
@@ -250,17 +247,13 @@ CREATE TABLE `receipt` (
 --
 
 INSERT INTO `receipt` (`receiptid`, `customerid`, `orderid`, `due-date`, `issudate`, `totalamount`, `image`) VALUES
-(8, 0, '', '', '', '', 'candidate_images/Chrysanthemum-jpg.jpg');
+(8, 0, '', '', '', '', 'candidate_images/Chrysanthemum-jpg.jpg'),
+(9, 0, 'fd', '', '', '', ''),
+(10, 0, 'fd', '', '', '', '');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `bottiqueowner`
---
-ALTER TABLE `bottiqueowner`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `boutique_order`
@@ -272,6 +265,12 @@ ALTER TABLE `boutique_order`
 -- Indexes for table `boutique_post`
 --
 ALTER TABLE `boutique_post`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -309,53 +308,45 @@ ALTER TABLE `receipt`
 --
 
 --
--- AUTO_INCREMENT for table `bottiqueowner`
---
-ALTER TABLE `bottiqueowner`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
 -- AUTO_INCREMENT for table `boutique_order`
 --
 ALTER TABLE `boutique_order`
   MODIFY `orderid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
 --
 -- AUTO_INCREMENT for table `boutique_post`
 --
 ALTER TABLE `boutique_post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1261;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1262;
 --
 -- AUTO_INCREMENT for table `order_product`
 --
 ALTER TABLE `order_product`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=132;
-
 --
 -- AUTO_INCREMENT for table `receipt`
 --
 ALTER TABLE `receipt`
-  MODIFY `receiptid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-COMMIT;
+  MODIFY `receiptid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
